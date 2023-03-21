3e269439f748f4e4e05c7f6493b77f0e1550addc
Siluu <silk0293@edu.zealand.dk>
2023-03-21 11:11:50 +0100

    CORS

--- DKRREST/Program.cs
+++ DKRREST/Program.cs
@@ -3,6 +3,10 @@ using DKRREST.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll", policy => { policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +19,8 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
